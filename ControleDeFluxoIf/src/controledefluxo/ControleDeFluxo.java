b68257c5586b64d63b8cb302bb9b5551268d1628
Diego Geronimo Onofre <solucoesdiego@gmail.com>
2015-09-04 15:39:50 -0300

    Estrutura de Controle If
    Código fonte do vídeo que explico como usar a estrutura de controle de
    fluxo if.

---
+++ ControleDeFluxoIf/src/controledefluxo/ControleDeFluxo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controledefluxo;
+
+/**
+ *
+ * @author Diego
+ */
+public class ControleDeFluxo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        if ( true == true )
+        {
+            int valor = 1;
+
+            if ( valor == 1 )
+            {
+                System.out.println("Valor é igual a um");
+                System.out.println("Outra mensagem.");
+                System.out.println("Terceira mensagem.");
+            }
+
+            if ( valor != 2 )
+            {
+                System.out.println("Segunda condição");
+                System.out.println("Segunda condição");
+                System.out.println("Segunda condição");
+            }
+        }
+    }
+    
+}
